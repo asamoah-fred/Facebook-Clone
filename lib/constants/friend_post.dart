94cead5c842d10804d9c6a4e94cef0bb89711ed0
asamoah-fred <asamoahgodfred42@gmail.com>
2022-03-05 17:30:17 +0000

    Profile Pic

--- lib/constants/friend_post.dart
+++ lib/constants/friend_post.dart
@@ -14,8 +14,73 @@ Widget FriendPost() {
             children: [
               Container(
                 width: 390,
-                height: 50,
-                color: Colors.red,
+                height: 60,
+                // color: Colors.red,
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 60,
+                          height: 60,
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(40),
+                                border: Border.all(
+                                  color: Colors.brown,
+                                  style: BorderStyle.solid,
+                                ),
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    'images/autumn_30.jpg',
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              width: 247,
+                              height: 20,
+                              color: Colors.pink,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              width: 247,
+                              height: 40,
+                              color: Colors.green,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 80,
+                          height: 60,
+                          color: Colors.brown,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
